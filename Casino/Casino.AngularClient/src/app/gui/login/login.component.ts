d3c2024e9b84d50b8c9fb8272e7f67ad4d7346cc
Krzysztof Szklarczyk <ks303240@student.polsl.pl>
2024-06-25 16:02:13 +0200

    delete old angular project, login screen, routing works

--- Casino/Casino.AngularClient/src/app/gui/login/login.component.ts
+++ Casino/Casino.AngularClient/src/app/gui/login/login.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -9,27 +10,32 @@ import { NgForm } from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  invalidLogin: boolean = false; //dodany !
+  form!: FormGroup;
+  invalidLogin: boolean = false; 
+  isLoginMode: boolean = true;
+
   //credentials: LoginModel = {emailAddress:'', password:''};
 
   constructor(private router: Router, private http: HttpClient) { }
 
-  // login = ( form: NgForm) => {
-  //   if (form.valid) {
-  //     this.http.post<AuthenticatedResponse>("http://localhost:5080/api/account/login", this.credentials, {
-  //       headers: new HttpHeaders({ "Content-Type": "application/json"})
-  //     })
-  //     .subscribe({
-  //       next: (response: AuthenticatedResponse) => {
-  //         const token = response.accessToken;
-  //         const refreshToken = response.refreshToken;
-  //         localStorage.setItem("accessToken", token); 
-  //         localStorage.setItem("refreshToken", refreshToken);
-  //         this.invalidLogin = false; 
-  //         this.router.navigate(["/"]);
-  //       },
-  //       error: (err: HttpErrorResponse) => this.invalidLogin = true
-  //     })
-  //   }
-  // }
+  login = ( form: NgForm) => {
+    if (form.valid) {
+      this.http.post<AuthenticatedResponse>("http://localhost:5080/api/account/login", this.credentials, {
+        headers: new HttpHeaders({ "Content-Type": "application/json"})
+      })
+      .subscribe({
+        next: (response: AuthenticatedResponse) => {
+          const token = response.accessToken;
+          const refreshToken = response.refreshToken;
+          localStorage.setItem("accessToken", token); 
+          localStorage.setItem("refreshToken", refreshToken);
+          this.invalidLogin = false; 
+          this.router.navigate(["/"]);
+        },
+        error: (err: HttpErrorResponse) => this.invalidLogin = true
+      })
+    }
+  }
+
+  
 }
