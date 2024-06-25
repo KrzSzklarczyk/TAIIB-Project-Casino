dbaf42a032c1d83a8a4e6072b059d18bf4084720
Przemysław Matera <przemat412@student.polsl.pl>
2024-06-25 17:40:56 +0200

    RegisterDziała

--- Casino/Casino.AngularClient/src/app/register/register.component.ts
+++ Casino/Casino.AngularClient/src/app/register/register.component.ts
@@ -1,10 +1,45 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { AuthenticatedResponse } from '../models/authenticated-response';
+import { UserRegisterRequestDTO } from '../models/register-model';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
+
 export class RegisterComponent {
+  form!: FormGroup;
+  invalidLogin: boolean = false;
+  isLoginMode: boolean = true;
+  credentials: UserRegisterRequestDTO = {login: '', password :'', nickName:'', avatar:'', email:''};
+
+
+  constructor(private router: Router, private http: HttpClient) { }
+
+  rejestracja = ( form: NgForm) => {
+    if (form.valid) {
+      this.http.post<AuthenticatedResponse>("https://localhost:7063/Account/register", this.credentials, {
+        headers: new HttpHeaders({ "Content-Type": "application/json"})
+      })
+      .subscribe({
+        next: (response: AuthenticatedResponse) => {
+          const token = response.accessToken;
+          const refreshToken = response.refreshToken;
+          localStorage.setItem("accessToken", token);
+          localStorage.setItem("refreshToken", refreshToken);
+          this.invalidLogin = false;
+          this.router.navigate(["/"]);
+        },
+        error: (err: HttpErrorResponse) => this.invalidLogin = true
+      })
+    }
+  }
+
+
 
 }
