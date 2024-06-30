ae5c13e3f8a153a01ef8a61df6a83e80ef6b27c8
Przemysław Matera <przemat412@student.polsl.pl>
2024-06-30 16:52:01 +0200

    Dodawanie Creditsów

--- Casino/Casino.AngularClient/src/app/gui/user-panel/transaction/transaction.component.ts
+++ Casino/Casino.AngularClient/src/app/gui/user-panel/transaction/transaction.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TransactionService } from '../../../services/transaction.service';
 
 @Component({
   selector: 'app-transaction',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './transaction.component.css'
 })
 export class TransactionComponent {
+  constructor(private transactionService:TransactionService){}
+  credits: number=0;
+  
+addCredits() {
+  if (this.credits !== null && this.credits > 0) 
+this.transactionService.addCredits(this.credits);
+}
 
 }
