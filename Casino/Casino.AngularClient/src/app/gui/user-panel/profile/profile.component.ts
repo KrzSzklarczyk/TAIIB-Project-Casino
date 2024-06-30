252735c89ff5871ea8c0e256b11a7f1822bd9d14
Przemysław Matera <przemat412@student.polsl.pl>
2024-07-01 00:22:50 +0200

    Profil full

--- Casino/Casino.AngularClient/src/app/gui/user-panel/profile/profile.component.ts
+++ Casino/Casino.AngularClient/src/app/gui/user-panel/profile/profile.component.ts
@@ -7,6 +7,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../../../services/user.service';
 import { AuthService } from '../../../services/auth.service';
 import { UserChangeDTO } from '../../../models/userChangeDto';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -21,14 +22,17 @@ export class ProfileComponent {
   showChangePasswordForm = false;
   showChangeAvatarForm = false;
   changePasswordForm: FormGroup;
+  showDeleteForm=false;
+  
   
   //form!: FormGroup;
   //validateHTML = this.userService.validateHTML.bind(this.userService)
 
-  constructor(private fb: FormBuilder, private formBuilder: FormBuilder, private http: HttpClient, private userService: UserService, private authService: AuthService) { 
+  constructor(private fb: FormBuilder, private formBuilder: FormBuilder, private http: HttpClient, private userService: UserService, private authService: AuthService,private router:Router) { 
     this.changePasswordForm = this.fb.group({
      
-      newPassword: ['', [Validators.required, Validators.minLength(6)]]
+      newPassword: ['', [Validators.required, Validators.minLength(6)]],
+      newAvatar: ['']
     });
   }
 
@@ -53,27 +57,57 @@ export class ProfileComponent {
   };
 
   changeAvatar() {
-    // Logic to change avatar
-    console.log('Change Avatar clicked');
-    this.getUserInfo();
+    
+      const awa = this.changePasswordForm.get('newAvatar')?.value;
+      this.cred.accessToken = localStorage.getItem('accessToken') ?? '';
+        this.cred.refreshToken = localStorage.getItem('refreshToken') ?? '';
+        
+   const us:UserChangeDTO = { token:this.cred, cos : awa}
+        if (this.cred.accessToken == '' || this.cred.refreshToken == '') {
+         
+         return
+        
+         }
+        
+        this.http.put<boolean>("https://localhost:7063/Account/ChangeAvatar", us, {
+            headers: new HttpHeaders({ "Content-Type": "application/json"})
+          })
+          .subscribe({
+            next: (response: boolean) => {
+             console.log("testtttt")
+             this.getUserInfo();
+             return
+            },
+            error: (err: HttpErrorResponse) => {return}
+          }); 
+          this.getUserInfo();
+      
   }
 
   toggleChangePasswordForm() {
     this.getUserInfo();
     this.showChangePasswordForm = !this.showChangePasswordForm;
     this.showChangeAvatarForm = false;
+    this.showDeleteForm=false;
   }
 
   toggleChangeAvatarForm() {
     this.getUserInfo();
     this.showChangeAvatarForm = !this.showChangeAvatarForm;
     this.showChangePasswordForm = false;
+    this.showDeleteForm=false;
+  }
+  toggleDeleteForm() {
+    this.getUserInfo();
+    this.showDeleteForm = !this.showDeleteForm;
+    this.showChangePasswordForm = false;
+    this.showChangeAvatarForm=false;
   }
 
   changePassword() {
     
     if (this.changePasswordForm.valid) {
-      console.log("test")
+    
       const newPassword = this.changePasswordForm.get('newPassword')?.value;
       this.cred.accessToken = localStorage.getItem('accessToken') ?? '';
         this.cred.refreshToken = localStorage.getItem('refreshToken') ?? '';
@@ -99,14 +133,33 @@ export class ProfileComponent {
   }
 
   deleteAccount() {
-    // Logic to delete account
-    console.log('Delete Account clicked');
-    this.getUserInfo();
+    this.cred.accessToken = localStorage.getItem('accessToken') ?? '';
+    this.cred.refreshToken = localStorage.getItem('refreshToken') ?? '';
+
+    if (this.cred.accessToken == '' || this.cred.refreshToken == '') {
+    } else {
+      this.http
+        .put<boolean>(
+          'https://localhost:7063/Account/RemoveAcc',
+          this.cred,
+          {
+            headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+          }
+        ) 
+        .subscribe({
+          next: (response: boolean) => {
+            
+          },
+        });
+    }
+    this.logOut()
+    this.router.navigate(["/"]);
+
   }
 
   logOut() {
     // Logic to log out
-    console.log('Logout clicked');
+  
     this.getUserInfo();
     this.authService.logOut();
   }
