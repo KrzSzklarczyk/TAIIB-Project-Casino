cfb78eb84419b47ba821a001381b3341e9ff8d8a
Przemysław Matera <przemat412@student.polsl.pl>
2024-06-30 22:56:10 +0200

    Backend

--- Casino/Casino.BLL/DTO/UserChangeDTO.cs
+++ Casino/Casino.BLL/DTO/UserChangeDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Casino.BLL.Authentication;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Casino.BLL.DTO
 {
-    internal class UserChangeDTO
+    public class UserChangeDTO
     {
+       public UserTokenResponse token {  get; set; }
+        public string cos {  get; set; }
     }
 }
